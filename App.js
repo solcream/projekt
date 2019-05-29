ffab88c7676ae863be2fe2ee5f60dcec7857f8ea
solcream <47736779+solcream@users.noreply.github.com>
2019-05-29 11:47:12 +0200

    commit

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ super(props);
 this.state = {
 articles: [
   {
+    /*lägger in koden för bilderna */
 urIToImage: "https://source.unsplash.com/random/200×125/?furniture",
 titel: "Test novelty 1",
 description: "description of the test news",
@@ -20,14 +21,14 @@ description: "description of the seconds test news",
 componentDidMount() {
     fetch ("https://newsapi.org/v2/top-headlines?country=se&apiKey=ae24bb6763034855bf26908e36dcd782" )
       .then(function (response) {
-      // gör något med det som kom tillbaka
+      /* gör något med det som kom tillbaka*/
         if (response.status !==200) {
           throw Error( `state:${response.status}`);
         }
         return response.json()
     })
     .then( jsondata => {
-      // gör något med json-objektet
+      /* gör något med json-objektet*/
       this.setState({ articles: jsondata.articles })
     })
     .catch (error=> {
@@ -41,10 +42,21 @@ componentDidMount() {
 })
 } 
 
-render() {
+render() { 
   return (
+    <div className="App">
+      <header className="App-header">
+    <div>
+      </div>
+
+      <h1> Dagens nyheter</h1>
+      <h4>Här kan du läsa dagens nyheter</h4>
+      </header>
+    
+    
    <Nyhetslista
         minaArtiklar={this.state.articles} />
+        </div>
   );
   }
   }
